242f1b7dcf9ce9b0da23c44c0e28afba5f26a5e2
aditya0b0 <aditya0by0@gmail.com>
2026-08-16 14:51:57 +0200

    resest to default parameters as per data

--- finetune/finetune.py
+++ finetune/finetune.py
@@ -210,10 +210,10 @@ def main():
     parser = argparse.ArgumentParser(description='PyTorch implementation of pre-training of graph neural networks')
     parser.add_argument('--device', type=int, default=0,
                         help='which gpu to use if any (default: 0)')
-    parser.add_argument('--batch_size', type=int, default=128,
-                        help='input batch size for training (default: 128)')
-    parser.add_argument('--epochs', type=int, default=200,
-                        help='number of epochs to train (default: 200)')
+    parser.add_argument('--batch_size', type=int, default=32,
+                        help='input batch size for training (default: 32)')
+    parser.add_argument('--epochs', type=int, default=100,
+                        help='number of epochs to train (default: 100)')
     parser.add_argument('--lr_feat', type=float, default=0.001,
                         help='learning rate (default: 0.001)')
     parser.add_argument('--lr_pred', type=float, default=0.001,
